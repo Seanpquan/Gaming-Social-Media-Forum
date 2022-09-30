de13ceb2b327b8c080066f56f878232fc1311a14
seanpquan <seanpquan@gmail.com>
2022-09-30 12:59:59 -0700

    changed intro

--- mern-gaming-forum/src/components/introduction-page.component.js
+++ mern-gaming-forum/src/components/introduction-page.component.js
@@ -39,27 +39,28 @@ export default class IntroductionPage extends Component {
         }
         return (
             <div>
-                <h1>Welcome to PianoTrack, {this.getCookie("currentCookie")}</h1>
+                <h1>Welcome to Le Epic Forum, {this.getCookie("currentCookie")}!</h1>
                 <h3> What is this app for?</h3>
                 <p>
-                    PianoTrack tracks your piano practice progress in an easy-to-use app.
+                    Le Epic Forum is your one-stop shop for chatting and discussion with fellow gamers.
                 </p>
-                <h3>How to use this app</h3>
+                <h3>How to use this forum</h3>
                 <ul>
-                    <li>"All Logs" shows data for all songs</li>
-                    <li>"Song List" shows data for a specific song</li>
-                    <li>"Add Log" and "Add Song" are self-explanatory</li>
-                    <li>"PianoTrack" can be clicked at any time to see this instruction page</li>
+                    <li>"All Posts" shows all posts by all users</li>
+                    <li>"User List" shows all active users</li>
+                    <li>"Create Post" is self-explanatory</li>
+                    <li>"Le Epic Forum" can be clicked at any time to see this instruction page</li>
                 </ul>
                 <h3>Info about the creator of this app</h3>
                 <p>
-                    Self-driven, eager to learn, and passionate about computer science.
-                    2ndth year computer science major, loves programming, and always looking for opportunities!
-                    Hello! I'm a 2nd year computer science student at UBC, from Vancouver, Canada.
+                    Hello! I'm a 3rd year computer science student at UBC, from Vancouver, Canada.
                     Ever since I started programming in high school, I was always striving to explore
                     new tools and technologies, from individual technical endeavours, to collaborative
                     hackathon projects, to work experience in industry. Feel free to check out my resume, and if you have any
-                    questions, or just want to chat, don't hesitate to add me on LinkedIn. Thanks for visiting!
+                    questions, or just want to chat, don't hesitate to add me on LinkedIn @https://www.linkedin.com/in/seanquan/.
+                    Thanks for visiting!
+                    <br></br><br></br>
+                    This is my favourite rapper, Corey Struggs.  Died @ 5'7", 425 lbs.  RIP big guy.
                 </p>
                 <img src={picture} alt="alternatetext"></img>
             </div>
@@ -67,5 +68,3 @@ export default class IntroductionPage extends Component {
         )
     }
 }
-
-
