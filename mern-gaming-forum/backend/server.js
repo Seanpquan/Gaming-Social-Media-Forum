8697c94d79541cd562e9fcfe4e717e5e55d34336
seanpquan <seanpquan@gmail.com>
2022-09-29 22:28:26 -0700

    help me god ahh

--- mern-gaming-forum/backend/server.js
+++ mern-gaming-forum/backend/server.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "mern-gaming-forum", "build")))
+app.use(express.static(path.join(__dirname, "..", "build")))
 
 const uri = process.env.ATLAS_URI;
 mongoose.connect(uri, { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true }
